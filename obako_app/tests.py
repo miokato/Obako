5dcd2f7ac79b994217e043ac49bcae482b374cc5
miokato <miokato@me.com>
2017-07-18 21:48:29 +0900

    Add Parser class to tests

--- obako_app/tests.py
+++ obako_app/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+import pandas as pd
+import unittest
+from obako_app.tools.parser import Parser
 
-# Create your tests here.
+
+class ParserTest(unittest.TestCase):
+
+    def setUp(self):
+        path = 'data/akitaben.csv'
+        dic = pd.read_csv(path, index_col=0)
+        self.parser = Parser(dic)
+
+    def test_parse(self):
+        sent = 'こんばんは。今日は寒いですね。'
+        parsed = self.parser.parse(sent)
+        self.assertEqual(type(parsed), list)
+
+
+
